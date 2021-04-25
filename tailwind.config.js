b63b36b8dd3d771dd761687ca9cb0df285b8a7b0
Henri F <82778679+HenzF@users.noreply.github.com>
2021-04-25 20:43:46 +0300

    Revert "Merge develop into master"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,6 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-    fontFamily: {
-      'sans': ['ui-sans-serif', 'system-ui'],
-
-      'serif': ['ui-serif', '"Courier New"'],
-
-      'mono': ['ui-monospace', 'SFMono-Regular'],
-
-      'body': ['Open Sans'],
-     }
   },
   variants: {
     extend: {},
